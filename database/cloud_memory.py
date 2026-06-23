fe93dcf72c860b487f2dc3a4d5e3b3897087fee3
Amr-KhaIid <118640059+Amr-KhaIid@users.noreply.github.com>
2026-06-23 17:08:17 +0300

    Update cloud_memory.py

--- database/cloud_memory.py
+++ database/cloud_memory.py
@@ -6,21 +6,11 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # رابط قاعدة البيانات الخاصة بك
-DATABASE_URL = 'https://hybrid-ai-memory-default-rtdb.firebaseio.com/' 
+DATABASE_URL = '' 
 
 #  دمج مفاتيح السحابة مباشرة كـ Dictionary لحمايتها عند تحويل البرنامج لـ exe
 FIREBASE_DICT = {
-  "type": "service_account",
-  "project_id": "hybrid-ai-memory",
-  "private_key_id": "a8818634636f28c8d380a007cf6c9458d0028eb3",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDA64+zvJpn2PLF\nD30VAmC9ThwA256Ho5vNk/xPzd8XSD3YtPxd0lHiw5YoTK1V9FkZTQJpysb0EZ7c\nnS/AaztWprwEFM3VbbUImf9joDNIc7qFXelQU1i+ija0AlC6tt/qC7OTtNr+OQMn\naLzHawWxEc1GoAcW2FwcCUKEa0fxkijrdbvgtnhI+HSq9EUnxtcl9ECh7fUDsOiS\nHHygz6lDsvGt+5ZCkt7Mngi1kxU762ci6oq1b4P6aqgHd4FR/M0YxGv8OGf5QEcu\nOBAHNQ2uiKa9vpFP7khg/O3JysH8reCm80+kev0fU+t9dAtq/yMrsgFW9iiBgFfR\n2QUunjktAgMBAAECggEAHiq3pdtwKZz7GWV6obZlejSknF27RGqJCnSBl9kRcYqS\nYTir3d/tfhAngtW6pR44cIRiRDi5M6EtvkG2Hdimxr372IeISD+Hd3jdPq7mVgYC\nvKgau7zMu77RHwTdqtS93tNFmWtGc0pm+9qc6nIJyc6G0uxjL2dVv+ySwjIkZdm3\nIu03n2wU1dHqWWILZmxAArWok36zRnPocvnmypr2YhgTYcUC2TMfXreFQa2uHqmD\nMPXHt2uHLKUZMx4Vmtwd8IPofYbOwdVVe8K9bKHqMoTWDUAfp3ngAKT54uZT+pOx\nxFBuzvNGdwrq5aOYXm1FwNSjZ+8YaPB+18vQLLQ1AQKBgQD5sVifSumbqT4UzkhH\n8Z1fu/mx4ouos5jC8q6nWbvzoTDk0gqjMt78r3Vn5u6RWjx9zIhh3DgEnSk3cc4V\nmsvNuKPNJhV+8fsaBnKjdp7St0Xn9TNCunwxuNkjMMuwHYCOsQwLf9Qz0AaYlzCw\n5f7WkO6KDxr9JxqAqL92AidcJQKBgQDFyxdzzRCApfRTMH8axtuRBR2l5pJKldw4\nosSFbWc4NNUMadIO4mOII2GvKn9ZZ/zN5aWwnMhrYCWD9s3GOKWvuM/bwMVGvTnN\nZx25uF9NcMo5op6RvcairzW20PKB9irQQvz4nhrCqDKyNgnZK4ewX7/2d45rs7SV\nmr6hi+FWaQKBgQCt6a9va88gg5XhCfjgW1Kgzp3RH5jkzQrWpg+uMlsuCxSyG/Ya\ny6Dy4Qbmcrux8+b0PBS2DJvb4tdFbff7plDTngpBJoiMXeDmtJz+a+2dmNeRA4FL\ntfYw764Vy7Pjm/jh6kEYdVWJLRibCZt5awi/zDzJXlIPB0B3YpHQkRl3HQKBgQDA\nfA5MsYmXyOjmdFGdm1xd2t6pbqN7Vi/EJhKdzoQe3LtgozK7LXGzMMuFjhP0zA/n\nx87g+xLH+/9GV06V0tbbFT2jHAxf9RJlZC43aMfGMzd5s1ohds/xzhS13s3Bz/CG\nUEqjICnmuYzshU48O/KGoAfOMTc2dOIOnVNbHyeh0QKBgQCsrA3t/oHKNRl7PPY4\neALVrqkRzvpVR7WD8NoV0URt7Njc4y43n5q2oKiNOLjPZlpWGacU9HpYy2druqR9\ndtX4lk91ELp9glwCZpqFfVj1ftNNGIrwxB0qoFYrS7vzJfWYVA43WTB4tSgaDe+4\nd8fzCRRlJJA9+XCPPTr1MBaM8g==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@hybrid-ai-memory.iam.gserviceaccount.com",
-  "client_id": "103232188913242734895",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40hybrid-ai-memory.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+  
 }
 
 def init_firebase():
